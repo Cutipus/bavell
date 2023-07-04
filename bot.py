05f5b5540ce25df79d7f3eac9add24aa85b9f483
Mick <Cutipus@protonmail.com>
2023-07-05 02:24:58 +0300

    Abstracted registered users under a separate class to drastically simplify

--- bot.py
+++ bot.py
@@ -40,70 +40,28 @@ class Bot(commands.Bot):
         intents.presences = True
         super().__init__(command_prefix="!", intents=intents)
 
-    #def on_ready(self):
-    #    logging.info("Bot ready")
-
-
 class Teacher(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
         self.dictionary = Dictionary(PATH_TO_DB)
-        self.users: set[int] = set()
-        self.online_users: set[int] = set()
-        self.user_tasks: dict[int, asyncio.Task] = dict()
-        self.user_events: dict[int, asyncio.Queue] = dict()
-
-    #@commands.Cog.listener()
-    #async def on_ready(self):
-    #    pass # initialize logic engine
-
-    async def user_daemon(self, userid: int):
-        self.users.add(userid)
-        user: discord.User = self.bot.get_user(userid)
-        task: discord.Task = None
-
-        await user.send('Welcome to the training program.')
-
-        try:
-            while True:
-                event = await self.user_events[userid].get()
-                logging.debug(f"Received user event from {user.name}: {event}")
-                if event == USER_ONLINE:
-                    await user.send("I see you're online; Why don't we start with some questions?")
-                    assert task is None
-                    task = asyncio.create_task(self.interval_based(user))
-                elif event == USER_OFFLINE:
-                    task.cancel()
-                    task = None
-                    pass # stop sending messages
-        except asyncio.CancelledError:
-            task.cancel()
-            raise
-    
-    async def interval_based(self, user):
-        while True:
-            await user.send(self.dictionary.generate_question())
-            await asyncio.sleep(60)
+        self.users: dict[int, User] = dict()
 
     @commands.Cog.listener()
     async def on_presence_update(self, before, after):
         userid = after.id
-        if userid not in self.users:
+        user: User = self.users.get(userid)
+        if not user:
             return
+
         if before.status != discord.Status.online and after.status == discord.Status.online:
-            if userid in self.online_users: # Discord sends two presence updates for some reason, ignoring second
-                return
-            self.online_users.add(userid)
+            user.online.set()
+        member = await commands.converter.memberconverter().convert(ctx, str(userid)) # discord.py can only fetch status for members
+        if member.status == discord.Status.online:
+            user.online.set()
             logging.info(f'{after.name} is now online')
-            await self.user_events[userid].put(USER_ONLINE)
         elif before.status == discord.Status.online and after.status != discord.Status.online:
-            if userid not in self.online_users: # ignoring duplicates, same as above
-                return
-            self.online_users.remove(userid)
+            user.online.clear()
             logging.info(f'{after.name} is now offline')
-            await self.user_events[userid].put(USER_OFFLINE)
-        else:
-            return
 
     @commands.command()
     async def hello(self, ctx, *, member: discord.Member = None):
@@ -112,39 +70,66 @@ class Teacher(commands.Cog):
 
     @commands.command()
     async def register(self, ctx):
-        user = ctx.author
-        if user.id in self.users:
-            await ctx.send(f'{user.name} is already registered.')
+        userid: int = ctx.author.id
+        if userid in self.users.keys():
+            await ctx.send(f'{ctx.author.name} is already registered.')
             return
 
-        self.user_events[user.id] = asyncio.Queue()
-
-        member = await commands.converter.MemberConverter().convert(ctx, str(user.id))
+        user = User(ctx.author, self)
+        member = await commands.MemberConverter().convert(ctx, str(userid)) # discord.py can only fetch status for members
         if member.status == discord.Status.online:
-            await self.user_events[user.id].put(USER_ONLINE)
-        self.user_tasks[user.id] = asyncio.create_task(self.user_daemon(ctx.author.id))
-        logging.debug(f"Added user task: {self.user_tasks[user.id]}")
-        await ctx.send(f'Registered {user.name}.')
+            user.online.set()
+
+        await user.send('Welcome to the training program. Whenever you are online new exercises will be sent.')
         logging.info(f"Registered {user.name} successfully.")
+        self.users[userid] = user
 
     @commands.command()
     async def unregister(self, ctx):
-        user = ctx.author
-        if user.id not in self.users:
-            await ctx.send(f'{user.name} is not registered.')
+        userid: int = ctx.author.id
+        if userid not in self.users.keys():
+            await ctx.send(f'{ctx.author.name} is not registered.')
             return
 
-        self.user_tasks[user.id].cancel()
-        del self.user_tasks[user.id]
-        del self.user_events[user.id]
-        self.users.remove(user.id)
+        user = self.users[userid]
+        del self.users[userid]
+
         await ctx.send(f'Unregistered {user.name}.')
         logging.info(f'Unregistered {user.name} successfully.')
 
 
+class User:
+    '''A registered user. Adds missing Status for discord.User'''
+    def __init__(self, discorduser: discord.User, teacher: Teacher):
+        self._discorduser = discorduser
+        self.teacher = teacher
+
+        self.interval: int = 120
+        self.online: asyncio.Event = asyncio.Event()
+
+        logging.debug("creating task")
+        self.task: asyncio.Task = asyncio.create_task(self.interval_based())
+
+    async def interval_based(self):
+        logging.info('Started interval-based question daemon')
+        while True:
+            logging.debug('Waiting for user to come online')
+            await self.online.wait()
+            logging.debug('Sending question')
+            await self.send(self.teacher.dictionary.generate_question())
+            logging.debug('Sleeping for interval')
+            await asyncio.sleep(self.interval)
+
+    def __del__(self):
+        self.task.cancel()
+
+    def __getattr__(self, attr):
+        return getattr(self._discorduser, attr)
+
 
 async def main():
-    logging.basicConfig(filename='everything.log', level=10)
+    #logging.basicConfig(filename='everything.log', level=10)
+    logging.basicConfig(level=10)
     logging.info("Loading environment variables")
     load_dotenv()
     BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -160,13 +145,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
-
-
-class user:
-    id = ...# user id
-    profile = ...# questionaire profile - how many questions at what difficulty
-    schedule = ... # when to send questionaire
-    submitted_data = ...# new words, associations, pictures, sentences
-    reports = ... # report for each questionaire
